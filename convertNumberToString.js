19efa72fe1cab8baa99ebaab9108b097957658a1
ashwinihegde28 <ashwinihegde28@gmail.com>
2024-02-18 14:46:58 -0500

    COnverting number to String in different ways

--- convertNumberToString.js
+++ convertNumberToString.js
@@ -7,3 +7,15 @@ console.log("Using String cast the datatype of a is : ", typeof StringTonum2);
 
 const StringTonum3 = "" + a;
 console.log("Using empty string the datatype of a is : ", typeof StringTonum3);
+
+let numberString = a.toLocaleString();
+console.log(
+  "Using toLocaleStrin the datatype of a  is : ",
+  typeof numberString
+);
+
+let variable_name = "${a}";
+console.log(
+  "Using toLocaleStrin the datatype of a  is : ",
+  typeof variable_name
+);
